62ca2eefcced1815180a70934a76b8998dc100d8
suraj232000suman <surajsuman232000@gmail.com>
2022-12-20 17:49:24 +0530

    installed ejs and setup views for home

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 8000;
 
 app.use('/',require('./routes/index.js'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the Server : ${err}`);
