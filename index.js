b912b6f87f55117aa5c29ee65c402df0e4a145d9
suraj232000suman <surajsuman232000@gmail.com>
2022-12-20 17:27:47 +0530

    added users routes and controller

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 8000;
 
+app.use('/',require('./routes/index.js'));
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the Server : ${err}`);
